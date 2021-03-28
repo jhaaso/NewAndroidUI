908a90dcc5679356a3de60e2092ac385cccf2af4
jhaaso <justin.haas@cuw.edu>
2021-03-28 14:51:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/newandroidui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.newandroidui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    system.out.println("hello world");
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
